1730baa8f526e3e72ee1c6ba1936664c91bd645a
Lppetrov0 <demirando8@abv.bg>
2024-02-28 23:46:11 +0200

    bxbbxbxb

--- 2nd_assignment/4th.c
+++ 2nd_assignment/4th.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
-#define COLLUMN = 4
-#define ROWS = 50
-void func(int *arr){
-    int **p = &arr[COLLUMN][ROWS];
-    int *p = &arr[COLLUMN];
-    for(; *p != '\0'; *p++){
-      print("%d",*p);
-        for(;**p != '\0';**p++){
-            printf("%d",**p);
-        }
+#define COLLUMN 4
+#define MAX_ROWS 50
+void func(int arr[COLLUMN][MAX_ROWS],int rows){
+   
+    for(int i=0;i < rows;i++){
+      for(int j=0;j < COLLUMN;j++){
+            printf("%d",*(*(arr + j)+i));
+           }
+        printf("\n");
     }
 }
-int main(void){
-    arr[ROWS][COLLUMN] = {{1,2,3,4},{5,6,7,8,8},{9,10,11,12},{13,14,15,16}};
+int main(){
+    int arr1[COLLUMN][MAX_ROWS];
+    int r = 0;
+    printf("\nEnter amount of Rows:");
+    scanf("%d",&r);
+    for(int i = 0; i < COLLUMN;i++){
+        printf("Enter elemtents of %d collumn ",i+1);
+        for(int j = 0; j < r; j++){
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+   func(arr1,r);
+
 }
